ac537f1887e8b17a5abcbd01536dc4ee7bd9ce39
Sam <samuel.j.simmons@gmail.com>
2016-03-08 17:20:14 +1300

    set up directory and config

---
+++ src/js/book.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MeditationsAur from './meditations-aur';
+
+
+class Book extends React.Component {
+	
+	render() {
+		
+		return (
+			<div>
+				<MeditationsAur />
+			</div>
+			
+		)
+	}
+}
+
+export default Book;
+
